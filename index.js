b18d1370e681748e98b93d8f1950d7121ed17bcd
SamH037 <samuel.ign01@gmail.com>
2021-08-22 19:49:57 -0300

    adicionando arquivos para a atividade

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("Bem vindo");
+let evento = "Senai Palestras"
+if( evento != "Senai Palestras")
+    console.log("Evento invalido, cadastro bloqueado");
+let data = "23/08/2021"
+    if (data => "23/08/2021")
+    console.log ("Data invalida, cadastro bloqueado");
+let cadastros = ["André", "João", "Pedro", "Luiz"];
+    if( cadastros.length < 100)
+    console.log( "digite seu nome");
+    console.log( "digite sua idade");
+let idade = 18
+    if( idade > 18)
+        console.log( "Cadastro concluído com sucesso!");
+    if( idade < 18)
+        console.log( "Idade não permitida!");
+    if( cadastros.length = 100)
+    console.log( "Cadastro não concluído, numero maximo de participantes atingido");
